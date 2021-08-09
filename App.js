4172edc61da0d388a21a7250cc73082b2c5a60d3
Techdyno BD <techdynobd@Techdynos-MacBook-Pro.local>
2021-08-09 13:42:51 +0600

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component, useState } from "react";
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  BackHandler,
+} from "react-native";
+import { WebView } from "react-native-webview";
+import LottieView from "lottie-react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class WebViewMoviezSpace extends Component {
+  constructor(props) {
+    super(props);
+    this.WEBVIEW_REF = React.createRef();
+    this.state = { visible: true };
+  }
+
+  hideSpinner() {
+    this.setState({ visible: false });
+  }
+  showSpinner() {
+    this.setState({ visible: true });
+  }
+  componentDidMount() {
+    BackHandler.addEventListener("hardwareBackPress", this.handleBackButton);
+  }
+  componentWillUnmount() {
+    BackHandler.removeEventListener("hardwareBackPress", this.handleBackButton);
+  }
+  handleBackButton = () => {
+    if (this.state.canGoBack) {
+      this.WEBVIEW_REF.current.goBack();
+      return true;
+    }
+  };
+  onNavigationStateChange(navState) {
+    this.setState({
+      canGoBack: navState.canGoBack,
+    });
+  }
+
+  onLoadStateChange(event) {
+    this.setState({
+      visible: false,
+    });
+  }
+  render() {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <StatusBar hidden />
+        <WebView
+          source={{ uri: "https://ringme.com.bd/" }}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          ref={this.WEBVIEW_REF}
+          onLoadStart={() => this.showSpinner()}
+          onLoad={() => this.hideSpinner()}
+          onNavigationStateChange={this.onNavigationStateChange.bind(this)}
+        />
+        {this.state.visible && (
+          <View style={styles.ActivityIndicatorStyle}>
+            <LottieView
+              style={{ backgroundColor: "#fff" }}
+              source={require("./assets/loader.json")}
+              autoPlay
+              loop
+            />
+          </View>
+        )}
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  ActivityIndicatorStyle: {
+    position: "absolute",
+
+    flex: 1,
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "auto",
+    marginBottom: "auto",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    justifyContent: "center",
   },
 });
